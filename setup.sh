2449b352574a5229d314b7d14d21f86a35d8f7ce
Andrew Loeliger <loeliger@wisc.edu>
2025-06-12 08:31:35 -0500

    Add dataset extraction to setup command

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 python3 -m venv cms_opendata_exercises_env
 source cms_opendata_exercises_env/bin/activate
 python3 -m pip install -r requirements.txt
+
+tar -xcvf data/basic_files.tar.gz data/basic_files
+tar -xcvf data/intermediate_files.tar.gz data/intermediate_files
+tar -xcvf data/advanced_files.tar.gz data/advanced_files
