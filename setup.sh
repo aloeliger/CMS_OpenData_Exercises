1e843538cfeb582c7ffa89bc028a4ccad859da46
Andrew Loeliger <loeliger@wisc.edu>
2025-06-12 08:42:50 -0500

    fix extraction command for data files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Making python environment..."
 python3 -m venv cms_opendata_exercises_env
 source cms_opendata_exercises_env/bin/activate
 python3 -m pip install -r requirements.txt
 
-tar -xcvf data/basic_files.tar.gz data/basic_files
-tar -xcvf data/intermediate_files.tar.gz data/intermediate_files
-tar -xcvf data/advanced_files.tar.gz data/advanced_files
+echo "Setting up data files..."
+tar -xzvf data/basic_files.tar.gz data/basic_files
+tar -xzvf data/intermediate_files.tar.gz data/intermediate_files
+tar -xzvf data/advanced_files.tar.gz data/advanced_files
+
+echo "Done!"
